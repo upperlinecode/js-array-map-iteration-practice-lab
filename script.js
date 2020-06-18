899defaa9cd7fc28dd9b7c20df473322e260dcdb
Jeff <jolson615@gmail.com>
2020-06-17 21:03:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,25 +26,28 @@ console.log("Script Running!")
 
 //// PART B: Array Iteration
 
-//// 6. Here's an array of all of Alex's favorite foods. For each food in the array, print out a statement that says "I really love _____" and then fill in the blank with each.
+//// 6. Here's an array of all of Alex's favorite foods. For each food in the array, log out a statement that says "I really love _____" and then fill in the blank with each.
 // favorite_foods = ["Sushi", "Pizza", "Salmon", "Tamales dulces", "Yuca fries", "Apple pie", "Katsu curry", "Shrimp and grits", "Waffles"]
 
 
 
 //// 7. Now for each food in that array, let's be specific about how MUCH Alex likes them.
-////    For example, print out "Alex's #1 favorite food is Sushi" and then "Alex's #2 favorite food is Pizza" and so on until you've made an ordered list.
+////    For example, log out "Alex's #1 favorite food is Sushi" and then "Alex's #2 favorite food is Pizza" and so on until you've made an ordered list.
 ////    This can be done a number of different ways, but the easiest is probably by iterating over each index in the favorite_foods array using a function called "range". Try to find it in the documentation.
 
 
 
 
-//// 8. Below is a massive array of numbers. Iterate over the array and print out only the even ones.
+//// 8. Below is a massive array of numbers. Iterate over the array and log out only the numbers less than 100. 
 // many_numbers = [109, 141, 44, 51, 133, 366, 339, 248, 226, 321, 97, 195, 245, 252, 238, 1, 366, 47, 189, 91, 148, 88, 194, 106, 5, 128, 165, 337, 380, 181, 143, 95]
 
 
 
-//// 9. Below is a array of 99 names. Iterate over the array and find the longest name in the whole thing.
-////    Then print out "The longest name in the array is ______. It has ___ letters in it."
+//// 9. Now iterate over that same list, but log out only the even ones. 
+
+
+
+//// 10. Below is a array of 99 names. Iterate over them all and only log out names with more than 7 characters.
 // many_names = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana",
 //   "Vance","Melinda","Tarik","Risa","Jordan","Camilla","Karly","Baker","Adena","Calvin","Kendall","Nasim","Kellie","Dana","Rhoda","Linus","Tyler","Ahmed","Dante","Shay","Lael","Tana","Claudia","Chadwick","Tara",
 //   "Fulton","Justine","Malcolm","Rowan","Christopher","Ciaran","Ivan","Hiram","Blake","Colton","Nathaniel","Moses","Cynthia","George","Ignacia","Chanda","Wyatt","Amethyst","Vladimir","Adam","Boris","Joseph","Scarlett","Kieran","Curran",
@@ -52,7 +55,14 @@ console.log("Script Running!")
 
 
 
-//// 10. Take that same array and sort it into two arrays: one array of long names, which have more than 6 characters, and one array of short names which have 6 or fewer characters. Then print out both arrays.
+
+//// 11. Iterate over the many_names array and find the longest name in the whole thing.
+////    Then print out "The longest name in the array is ______. It has ___ letters in it."
+
+
+
+
+//// 12. Take that same array and sort it into two arrays: one array of long names, which have more than 6 characters, and one array of short names which have 6 or fewer characters. Then print out both arrays.
 
 
 
@@ -60,13 +70,13 @@ console.log("Script Running!")
 
 //// Part C: CHALLENGES:
 
-//// 11 (CHALLENGE): Log out every number from 1-100 which is a multiple of 3 AND an even number.
+//// 13 (CHALLENGE): Log out every number from 1-100 which is a multiple of 3 AND an even number.
 
 
-//// 12 (Challenge): Log out the first 40 numbers in the fibbonacci sequence
+//// 14 (Challenge): Log out the first 40 numbers in the fibbonacci sequence
 
 
-//// 13 (Challenge): Log out the sum of every integer from 1-1000
+//// 15 (Challenge): Log out the sum of every integer from 1-1000
 
 
-//// 14 (Challenge): In the super long list of names, figure out if there are any doubles.
+//// 16 (Challenge): In the super long list of names, figure out if there are any doubles.
