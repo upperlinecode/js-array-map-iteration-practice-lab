b83f853c176ea7d24c89be45441d1741f5ae5804
Melanie <46007892+melwritescode@users.noreply.github.com>
2021-07-01 11:50:35 -0600

    Update script.js
    - Add const declaration to array variables
    - Change Py range function to JS forEach

--- script.js
+++ script.js
@@ -27,19 +27,19 @@ console.log("Script Running!")
 //// PART B: Array Iteration
 
 //// 6. Here's an array of all of Alex's favorite foods. For each food in the array, log out a statement that says "I really love _____" and then fill in the blank with each.
-// favorite_foods = ["Sushi", "Pizza", "Salmon", "Tamales dulces", "Yuca fries", "Apple pie", "Katsu curry", "Shrimp and grits", "Waffles"]
+// const favorite_foods = ["Sushi", "Pizza", "Salmon", "Tamales dulces", "Yuca fries", "Apple pie", "Katsu curry", "Shrimp and grits", "Waffles"]
 
 
 
 //// 7. Now for each food in that array, let's be specific about how MUCH Alex likes them.
 ////    For example, log out "Alex's #1 favorite food is Sushi" and then "Alex's #2 favorite food is Pizza" and so on until you've made an ordered list.
-////    This can be done a number of different ways, but the easiest is probably by iterating over each index in the favorite_foods array using a function called "range". Try to find it in the documentation.
+////    This can be done a number of different ways, but the simplest is probably by iterating over each index in the favorite_foods array using a function called "forEach". Try to find it in the documentation.
 
 
 
 
 //// 8. Below is a massive array of numbers. Iterate over the array and log out only the numbers less than 100. 
-// many_numbers = [109, 141, 44, 51, 133, 366, 339, 248, 226, 321, 97, 195, 245, 252, 238, 1, 366, 47, 189, 91, 148, 88, 194, 106, 5, 128, 165, 337, 380, 181, 143, 95]
+// const many_numbers = [109, 141, 44, 51, 133, 366, 339, 248, 226, 321, 97, 195, 245, 252, 238, 1, 366, 47, 189, 91, 148, 88, 194, 106, 5, 128, 165, 337, 380, 181, 143, 95]
 
 
 
@@ -48,7 +48,7 @@ console.log("Script Running!")
 
 
 //// 10. Below is a array of 99 names. Iterate over them all and only log out names with more than 7 characters.
-// many_names = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana",
+// const many_names = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana",
 //   "Vance","Melinda","Tarik","Risa","Jordan","Camilla","Karly","Baker","Adena","Calvin","Kendall","Nasim","Kellie","Dana","Rhoda","Linus","Tyler","Ahmed","Dante","Shay","Lael","Tana","Claudia","Chadwick","Tara",
 //   "Fulton","Justine","Malcolm","Rowan","Christopher","Ciaran","Ivan","Hiram","Blake","Colton","Nathaniel","Moses","Cynthia","George","Ignacia","Chanda","Wyatt","Amethyst","Vladimir","Adam","Boris","Joseph","Scarlett","Kieran","Curran",
 //   "Dalton","Paul","Phillip","Plato","Renee","Natalie","Barbara","Keiko","Oleg","Xerxes","Caesar","Kareem","Ahmed","Charles","Cyrus","Adria","Winifred","Pandora","Wynne","Simon","Wanda","Coby","Nolan","Marsden","Courtney"]
